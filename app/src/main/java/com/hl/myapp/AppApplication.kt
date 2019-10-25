835e35e789c12613c5f5a2dc6ac094bef2054128
michael <18817311356@163.com>
2019-10-25 16:02:14 +0800

    工程配置文件位置修改

--- app/src/main/java/com/hl/myapp/AppApplication.kt
+++ app/src/main/java/com/hl/myapp/AppApplication.kt
@@ -1,38 +1,26 @@
 package com.hl.myapp
 
-import com.hl.componentlogin.LoginObserver
 import com.hl.corelib.CoreApplication
 
 class AppApplication : CoreApplication() {
 
-    //TODO 初始化相关资源
     override fun onCreate() {
         super.onCreate()
 
-        //注册全局登陆监听 回调成功状态 然后读取配置信息 配置基本工程
-//        val loginSuccess = LoginType.LOGIN_SUCCESS
-//        println("loginSuccess is $loginSuccess")
+        if (BuildConfig.IS_NEED_APP_CANCEL.toBoolean()) {
+            //初始化appCancel模块内容
+        }
 
-        LoginObserver.loginResult.observeForever {
-            if (it == 1) {//登录成功
-                if (BuildConfig.IS_NEED_APP_CANCEL.toBoolean()) {
-                    //初始化appCancel模块内容
-                }
+        if (BuildConfig.IS_NEED_APP_IM.toBoolean()) {
+            //初始化appIM模块内容
+        }
 
-                if (BuildConfig.IS_NEED_APP_IM.toBoolean()) {
-                    //初始化appIM模块内容
-                }
+        if (BuildConfig.IS_NEED_APP_SITE.toBoolean()) {
+            //初始化appSite模块内容
+        }
 
-                if (BuildConfig.IS_NEED_APP_SITE.toBoolean()) {
-                    //初始化appSite模块内容
-                }
-
-                if (BuildConfig.IS_NEED_APP_SUBJECT.toBoolean()) {
-                    //初始化appSubject模块内容
-                }
-            } else {
-                //登录失败
-            }
+        if (BuildConfig.IS_NEED_APP_SUBJECT.toBoolean()) {
+            //初始化appSubject模块内容
         }
     }
 
